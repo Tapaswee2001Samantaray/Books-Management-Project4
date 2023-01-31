0ba9387dff3a91c554e4e80cb6d6c282747281ac
Sagar <sagarmaan2015@gmail.com>
2023-01-31 14:52:53 +0530

    Project-4 Deployment

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -2,51 +2,53 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
     {
-    title : {
-        type : String,
-        required : true,
-        enum : ["Mr", "Mrs", "Miss"],
-        trim : true
-    },
-    name : {
-        type : String,
-        required : true,
-        trim : true
-    },
-    phone : {
-        type : String,
-        required : true,
-        unique : true,
-        trim : true
-    },
-    email : {
-        type : String,
-        required : true,
-        unique : true,
-        trim : true,
-        lowercase : true
-    },
-    password : {
-        type : String,
-        required : true,
-        minLength : 8,
-        maxLength : 15,
-        trim : true
-    },
-    address : {
-        street : {
-        type : String, 
-        trim : true 
+        title: {
+            type: String,
+            required: true,
+            enum: ["Mr", "Mrs", "Miss"],
+            trim: true
         },
-        city : { 
-        type : String, 
-        trim : true 
+        name: {
+            type: String,
+            required: true,
+            trim: true
         },
-        pincode : { 
-        type : String, 
-        trim : true 
-        }
-    },
-}, { timestamps : true })
+        phone: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 8,
+            maxLength: 15,
+            trim: true
+        },
+        address: {
+            street: {
+                type: String,
+                trim: true
+            },
+            city: {
+                type: String,
+                trim: true
+            },
+            pincode: {
+                type: String,
+                trim: true
+            }
+        },
+    }, { timestamps: true })
 
-module.exports = mongoose.model( 'userData' , userSchema )
+
+    
+module.exports = mongoose.model('userData', userSchema)
